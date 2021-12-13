c0fd74e7df5a8d2227856f66a24e94d0d08edb17
Kevin Bos <2154558@talnet.nl>
2021-12-13 10:34:44 +0100

    opgave 2.6 af

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$string = "goedemorgen";
+$float = 10;
+$boolean = true;
+$null = null;
+
+echo gettype($string);
+echo gettype($float);
+echo gettype($boolean);
+echo gettype($null);
+?>
+
+</body>
+</html> 
