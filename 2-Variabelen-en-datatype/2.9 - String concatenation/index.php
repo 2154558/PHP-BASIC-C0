89c94865058b763db1d3c97080a19ccf58139060
Kevin Bos <2154558@talnet.nl>
2021-12-13 11:04:44 +0100

    opgave 2.9 af

---
+++ 2-Variabelen-en-datatype/2.9 - String concatenation/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$leeftijd = 16;
+$voornaam ="kevin";
+
+echo "Jouw naam is " . $voornaam . " en je bent" . $leeftijd . " jaar oud.";
+?>
+
+</body>
+</html> 
