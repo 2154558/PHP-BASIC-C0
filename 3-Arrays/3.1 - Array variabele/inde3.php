8755afd4b1a9136505d8d29403f9fb4f4b4336ed
Kevin Bos <2154558@talnet.nl>
2021-12-13 11:26:29 +0100

    opgave 3.1 af

---
+++ 3-Arrays/3.1 - Array variabele/inde3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myMovies = array("Lord of the rings, the fellowship of the ring. " . "Lord of The Rings, The Two Towers " . "Lord of The Rings, The Return of The King");
+echo $myMovies[0]
+?>
+
+</body>
+</html> 
