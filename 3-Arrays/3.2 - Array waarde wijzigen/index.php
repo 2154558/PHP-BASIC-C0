4e89c145da4afdafd106272025fb0c40d07dddc4
Kevin Bos <2154558@talnet.nl>
2021-12-13 11:41:24 +0100

    opgave 3.2 af

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myDognames = ['dikzak ' . 'vetklep ' . 'lui zak hooi.....'];
+echo $myDognames[0];
+
+$myDognames[1] = 'dikzak ' . 'kuthond ' . 'lui zak hooi';
+echo $myDognames[1];
+
+
+
+?>
+
+</body>
+</html> 
