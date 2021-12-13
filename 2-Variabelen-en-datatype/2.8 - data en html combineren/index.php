3063aab86f526905edd3beb0e6703d8390daaaea
Kevin Bos <2154558@talnet.nl>
2021-12-13 10:55:42 +0100

    opgave 2.8 af

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$naam_1 = "voornaam 1";
+$naam_2 = "voornaam 2";
+$naam_3 = "voornaam 3";
+
+echo "<ul> $naam_1</ul> <ul>$naam_2</ul>  <ul>$naam_3</ul>"
+?>
+
+</body>
+</html> 
