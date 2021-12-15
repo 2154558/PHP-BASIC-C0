e79b65828d5311da14f3e64dfb952dcb5116b452
Kevin Bos <2154558@talnet.nl>
2021-12-15 10:33:26 +0100

    opgave 3.5 af

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getallen = ['676', '5345', '78657', '3',  '46', '21', '47'];
+        asort($getallen);
+        print_r($getallen);
+
+
+?>
+
+<?php
+$getallen_2 = ['676', '5345', '78657', '3',  '46', '21', '47'];
+        arsort($getallen_2);
+        print_r($getallen_2);
+?>
+</body>
+</html>
+
+
