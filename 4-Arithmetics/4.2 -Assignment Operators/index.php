5c6d0c50babfb70571a27efd2f284f1efcf1b9ad
Kevin Bos <2154558@talnet.nl>
2021-12-15 10:52:36 +0100

    opgave 4.2 af

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getal1 = (10);
+
+echo ($getal1 += 2);
+
+$getal1 = (10);
+
+echo  ($getal1 -= 2);
+
+$getal1 = (10);
+
+echo ($getal1 *= 2);
+
+$getal1 = (10);
+
+echo ($getal1 /= 2 );
+
+?>
+
+</body>
+</html>
