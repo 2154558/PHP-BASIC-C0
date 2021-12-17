2122851caacb90178bc89f69145f4c016fdab352
Kevin Bos <2154558@talnet.nl>
2021-12-17 12:51:26 +0100

    opgave 6.1 af

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function zetTweeWoordenAanElkaar(){
+    $woordje = ("hallo");
+    $zinnetje = ("een mooie blouwe planeet");
+        echo ($woordje);
+        echo ($zinnetje);
+}
+zetTweeWoordenAanElkaar();
+?>
+
+</body>
+</html>
