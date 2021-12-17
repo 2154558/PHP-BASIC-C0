9af7ac4eaa5dac4f3434f7d876352c73fa24a1dc
Kevin Bos <2154558@talnet.nl>
2021-12-17 13:21:49 +0100

    opgave 7.1 af

---
+++ 7-Working with files/7.1 - include/includes/menu.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<ul>kaas</ul>
+<ul>tomaat</ul>
+<ul>boter</ul>
+<?php
+
+?>
+
+</body>
+</html>
---
+++ 7-Working with files/7.1 - include/index.php
@@ -0,0 +1,13 @@
+
+
+<html>
+<head></head>
+<body>
+
+    <div class="menu">
+        <?php include 'includes/menu.php';?>
+    </div>
+
+</body>
+</html>
+
