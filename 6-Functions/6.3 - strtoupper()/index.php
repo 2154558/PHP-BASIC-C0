fc7adfc6e05b325b17fd7529f4ec08133fedf47c
Kevin Bos <2154558@talnet.nl>
2021-12-17 13:07:56 +0100

    opgave 6.3 af

---
+++ 6-Functions/6.3 - strtoupper()/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function telAlleWoorden(){
+    $zin = ("mijn blauwe auto is een mooie snelle wagen");
+    $zinlengte = strlen($zin);
+    echo $zinlengte;
+}
+telAlleWoorden();
+?>
+
+</body>
+</html>
