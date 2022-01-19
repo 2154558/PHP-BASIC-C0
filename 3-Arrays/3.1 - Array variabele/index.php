2336a6d209e1919cc8377d9218996602bbe6ea08
2154558 <90393278+2154558@users.noreply.github.com>
2022-01-19 09:59:45 +0100

    opgave 3.1 verbeterd

---
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$myMovies = array("Lord of the rings,the fellowship of the ring.",  "Lord of The Rings, The Two Towers ",  "Lord of The Rings, The Return of The King");
+print_r($myMovies)
+
+?>
+
+</body>
+</html> 
